6f19b6b87380afd0f70e319cec973bddef44c92f
VSMent <vsmentvs@gmail.com>
2020-09-13 21:36:08 +0300

    First build

---
+++ build/precache-manifest.15ca25aba354bbb62bf9071f38e24431.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c6fc0e7f149479d237758eebe445a90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af5c5321408ccaf581ed",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3f300b66348fbcac0d77",
+    "url": "/static/js/2.63609a6d.chunk.js"
+  },
+  {
+    "revision": "8f6943f43adc6ed8f1e24482bce62246",
+    "url": "/static/js/2.63609a6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af5c5321408ccaf581ed",
+    "url": "/static/js/main.02be445d.chunk.js"
+  },
+  {
+    "revision": "dab1ffbe3e6c67151ba7",
+    "url": "/static/js/runtime-main.10b3f103.js"
+  }
+]);
